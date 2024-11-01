e879d517791a60dfc11749366dd245cff39ea930
Quyendev <146425082+Grozay@users.noreply.github.com>
2024-11-01 18:52:25 +0700

    create cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,9 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+//những domain được phép truy cập vào tài nguyên của server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  //đẩy lên domain chính thức
+]
